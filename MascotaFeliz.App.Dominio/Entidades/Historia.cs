e35264cb651dde6928ec9634cdf41c56916269d9
OscarMLopezI <lopezibarraoscar89@gmail.com>
2022-09-22 22:53:45 -0500

    entidadhistoria

--- MascotaFeliz.App.Dominio/Entidades/Historia.cs
+++ MascotaFeliz.App.Dominio/Entidades/Historia.cs
@@ -7,7 +7,7 @@ namespace MascotaFeliz.App.Dominio
     
     public class Historia
     {   public int Id {get;set;}
-        public String FechaInicial {get;set;}
+        public Datetime FechaInicial {get;set;}
         public List<VisitaPyP> VisitasPyP {get;set;}
     }
 }
